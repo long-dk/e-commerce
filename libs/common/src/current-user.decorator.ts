86d2958ea9b7275e041628c3baf1f44965e701ae
Long CV <cvlongvn@gmail.com>
2026-06-14 14:22:19 +0700

    fix: wrong context with graphql

--- libs/common/src/current-user.decorator.ts
+++ libs/common/src/current-user.decorator.ts
@@ -1,9 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const CurrentUser = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    const user = request.user;
+  (data: string, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    const user = ctx.getContext().req.user;
     return data ? user?.[data] : user;
   },
 );
